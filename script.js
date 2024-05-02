7d85fe2f7a0dc4764df145ebbf1aa7743c1d6279
thumb22 <bogdankreminskyi1111@gmail.com>
2024-05-02 13:12:09 +0300

    \

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const TOKEN = ""
-const CHAT_ID = ""
+const TOKEN = "7181961801:AAF_chw2_DcSmrp1FvWE5McG1mJAeLbsd0Q"
+const CHAT_ID = "771451766"
 const URI_API = `https://api.telegram.org/bot${TOKEN}/sendMessage`
 const URI_API_DOCUMENT = `https://api.telegram.org/bot${TOKEN}/sendDocument`
 const success = document.getElementById('success')
@@ -61,3 +61,67 @@ document.addEventListener("DOMContentLoaded", function () {
 window.addEventListener("load", function (event) {
     document.querySelector('.avatar').classList.add('loaded')
 });
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     const contentDiv = document.querySelector('.content');
+//     const navLinks = document.querySelectorAll('.lang_switch');
+//     const loadingOverlay = document.querySelector('.loading-overlay');
+  
+//     const showLoadingOverlay = () => {
+//       loadingOverlay.style.opacity = '1';
+//     };
+  
+//     const hideLoadingOverlay = () => {
+//       loadingOverlay.style.opacity = '0';
+//     };
+  
+//     const loadScripts = (url) => {
+//       if (url.includes('about')) {
+//         document.querySelector('.btn').addEventListener('click', (e) => {
+//           console.log(e);
+//         });
+//       }
+//     };
+  
+//     const loadPage = (url) => {
+//       showLoadingOverlay();
+  
+//       fetch(url)
+//         .then(response => response.text())
+//         .then(html => {
+//           const parser = new DOMParser();
+//           const doc = parser.parseFromString(html, 'text/html');
+//           const newContent = doc.querySelector('.content').innerHTML;
+  
+//           contentDiv.classList.add('fade-out');
+  
+//           contentDiv.innerHTML = newContent;
+//           document.title = doc.title;
+  
+//           setTimeout(() => {
+//             contentDiv.classList.remove('fade-out');
+//             history.pushState({}, '', url);
+//             hideLoadingOverlay();
+//           }, 500);
+//         })
+//         .then(() => {
+//           loadScripts(url);
+//         });
+//     };
+  
+//     navLinks.forEach(el => {
+//       el.addEventListener('click', (e) => {
+//         e.preventDefault();
+  
+//         const url = e.currentTarget.getAttribute('href');
+//         loadPage(url);
+//       });
+//     });
+  
+//     loadPage(window.location.pathname);
+  
+//     window.addEventListener('popstate', () => {
+//       loadPage(window.location.pathname);
+//     });
+//   });
+  
