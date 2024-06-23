5245b70b5895e8fae6717d9de6bb824af184bc34
thumb22 <bogdankreminskyi1111@gmail.com>
2024-06-23 17:52:31 +0300

    data editions, code refactoring

--- script.js
+++ script.js
@@ -6,59 +6,59 @@ const success = document.getElementById("success");
 const successDocument = document.getElementById("success-document");
 
 document.getElementById("form").addEventListener("submit", function (e) {
-  e.preventDefault();
+    e.preventDefault();
 
-  let message = `<i><b>Сообщение!</b></i>\n`;
-  message += `<b>Отправитель: </b> ${this.name.value}\n`;
-  message += `<b>Почта: </b> ${this.email.value}\n`;
-  if (message) message += `<b>Сообщение: </b> ${this.message.value}`;
+    let message = `<i><b>Сообщение!</b></i>\n`;
+    message += `<b>Отправитель: </b> ${this.name.value}\n`;
+    message += `<b>Почта: </b> ${this.email.value}\n`;
+    if (message) message += `<b>Сообщение: </b> ${this.message.value}`;
 
-  axios
-    .post(URI_API, {
-      chat_id: CHAT_ID,
-      parse_mode: "html",
-      text: message,
-    })
+    axios
+        .post(URI_API, {
+            chat_id: CHAT_ID,
+            parse_mode: "html",
+            text: message,
+        })
 
-    // Message sent successfully
-    .then((res) => {
-      this.name.value = "";
-      this.email.value = "";
-      this.message.value = "";
-      Swal.fire({
-        title: "Success",
-        text: "Thank you for writing to me! I will answer you shortly!",
-        icon: "success",
-        confirmButtonText: "Ok",
-      });
-    })
+        // Message sent successfully
+        .then((res) => {
+            this.name.value = "";
+            this.email.value = "";
+            this.message.value = "";
+            Swal.fire({
+                title: "Success",
+                text: "Thank you for writing to me! I will answer you shortly!",
+                icon: "success",
+                confirmButtonText: "Ok",
+            });
+        })
 
-    // Message sent Error
-    .catch((err) => {
-      console.warn(err);
-      Swal.fire({
-        title: "Error!",
-        text: "Something went wrong!",
-        icon: "error",
-        confirmButtonText: "Ok",
-      });
-    })
+        // Message sent Error
+        .catch((err) => {
+            console.warn(err);
+            Swal.fire({
+                title: "Error!",
+                text: "Something went wrong!",
+                icon: "error",
+                confirmButtonText: "Ok",
+            });
+        })
 
-    // Message sending finished
-    .finally(() => {
-      console.log("Сообщение отправлено!");
-    });
+        // Message sending finished
+        .finally(() => {
+            console.log("Сообщение отправлено!");
+        });
 });
 // This script can used for sending messages and Files in Landing Pages | By TwoX Studio | All rights reserved.
 
 document.addEventListener("DOMContentLoaded", function () {
-  var allLinks = document.querySelectorAll(".info a, .card a");
+    var allLinks = document.querySelectorAll(".info a, .card a");
 
-  allLinks.forEach(function (link) {
-    link.setAttribute("target", "_blank");
-  });
+    allLinks.forEach(function (link) {
+        link.setAttribute("target", "_blank");
+    });
 });
 
 window.addEventListener("load", function (event) {
-  document.querySelector(".avatar").classList.add("loaded");
+    document.querySelector(".avatar").classList.add("loaded");
 });
